6ec6d3e7f049dd2322bf6852aedc66cb7618bf95
rtf48 <rtf48@cornell.edu>
2025-07-19 11:42:19 -0400

    form theoretically functional

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from "./pages/home/Home";
 function App() {
   return (
     <>
-      <Home />
+      <Home />  
     </>
   );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -36,16 +36,70 @@ function ResizingMessageInput() {
       </label>
       <textarea
         ref={textareaRef}
+        id="MessageInput"
         className="form-control"
         rows="8"
         onInput={handleInput}
         style={{ overflow: "hidden", resize: "none" }} 
+        name="message"
       />
     </>
   );
 }
 
+function FormBackend() {
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", "252ecc86-2653-48bb-8aa5-ececf348c499");
+    
+        const object = Object.fromEntries(formData);
+        const json = JSON.stringify(object);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+          },
+          body: json
+        }).then((res) => res.json());
+    
+        if (res.success) {
+          console.log("Success", res);
+        }
+      };
+
+      return (
+        <form onSubmit={onSubmit}>
+            <div className="mb-3">
+                <label htmlFor="NameInput" className="form-label">
+                    Name
+                </label>
+                <input type="text" className="form-control" id="NameInput" name="name"/>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="EmailInput" className="form-label">
+                  Email address
+                </label>
+                <input type="email" className="form-control" id="EmailInput" name="email" />
+              </div>
+              <div className="mb-3">
+                <ResizingMessageInput />
+              </div>
+              <div className="container text-center">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+              </div>
+            </form>
+      );
+}
+
 function ContactFormPortal() {
+
   return createPortal(
     <div className="modal fade" id="contactModal" tabIndex="-1">
       <div className="modal-dialog">
@@ -60,22 +114,7 @@ function ContactFormPortal() {
           </div>
 
           <div className="modal-body">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="EmailInput" className="form-label">
-                  Email address
-                </label>
-                <input type="email" className="form-control" id="EmailInput" />
-              </div>
-              <div className="mb-3">
-                <ResizingMessageInput />
-              </div>
-              <div className="container text-center">
-                <button type="submit" className="btn btn-primary">
-                  Submit
-                </button>
-              </div>
-            </form>
+            <FormBackend />
           </div>
         </div>
       </div>
